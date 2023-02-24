f2d7e23f8edc4a11b488060917299192bf6cac17
Juan Jimenez <juanjiimenez8@gmail.com>
2023-02-24 17:12:52 -0500

    Updates

---
+++ precache-manifest.817477d5883be61347a8070f367072fe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "99a3f2797abe05c296b7",
+    "url": "//DW2Actividad1/static/css/main.1b998b9a.chunk.css"
+  },
+  {
+    "revision": "99a3f2797abe05c296b7",
+    "url": "//DW2Actividad1/static/js/main.99a3f279.chunk.js"
+  },
+  {
+    "revision": "205849d973eaa941ab0f",
+    "url": "//DW2Actividad1/static/js/1.205849d9.chunk.js"
+  },
+  {
+    "revision": "7bb8756fb78eb5ed0455",
+    "url": "//DW2Actividad1/static/css/2.bfcaa3f4.chunk.css"
+  },
+  {
+    "revision": "7bb8756fb78eb5ed0455",
+    "url": "//DW2Actividad1/static/js/2.7bb8756f.chunk.js"
+  },
+  {
+    "revision": "4e824d821567fd7588bd",
+    "url": "//DW2Actividad1/static/js/runtime~main.4e824d82.js"
+  },
+  {
+    "revision": "7f477633ddd12f84284654f2a2e89b8a",
+    "url": "//DW2Actividad1/static/media/bootstrap-icons.7f477633.woff2"
+  },
+  {
+    "revision": "52196284de1fcb5b044f001a75482dba",
+    "url": "//DW2Actividad1/static/media/bootstrap-icons.52196284.woff"
+  },
+  {
+    "revision": "f0cc63899ca4b226896e832aeb9a1c8a",
+    "url": "//DW2Actividad1/index.html"
+  }
+];
